2b30ee7861b4a583cbd1d37721ac2a4063c2e834
Saurav Jaglan <75674561+saurav19apr@users.noreply.github.com>
2023-09-17 19:36:21 +0100

    Update HR Database Execise.sql

--- HR Database Execise.sql
+++ HR Database Execise.sql
@@ -26,6 +26,108 @@
 
 #14. select concat(FIRST_NAME,' ',LAST_NAME) as 'Full Name',SALARY,MANAGER_ID from hr.employees where MANAGER_ID  !=0;
 
+#15. select * from hr.employees where HIRE_DATE<'2002-06-21';
+
+#16. select FIRST_NAME,LAST_NAME,EMAIL,SALARY,MANAGER_ID from hr.employees where MANAGER_ID in (120,103,145)
+
+#17. select * from hr.employees where FIRST_NAME like '%D%' or FIRST_NAME like '%S%' or FIRST_NAME like '%N%' order by salary desc;
+
+#18. select concat(FIRST_NAME,' ',LAST_NAME) as 'Full Name', HIRE_DATE,COMMISSION_PCT,concat(EMAIL,' - ',PHONE_NUMBER) as 'Contact Details',SALARY from hr.employees where SALARY >11000 or PHONE_NUMBER like'______3%' order by FIRST_NAME desc;
+
+#19. select FIRST_NAME,LAST_NAME,DEPARTMENT_ID from hr.employees where  FIRST_NAME like '__s%';
+
+#20. select EMPLOYEE_ID,FIRST_NAME,JOB_ID, DEPARTMENT_ID from hr.employees where DEPARTMENT_ID not in (50,30,80);
+
+#21. select EMPLOYEE_ID,FIRST_NAME,JOB_ID, DEPARTMENT_ID from hr.employees where DEPARTMENT_ID  in (30,40,90);
+
+#22. select EMPLOYEE_ID from hr.job_history  group by EMPLOYEE_ID having  count(*)>=2;
+
+#23. select JOB_ID,count(*) as 'Number of Emp',sum(SALARY) as 'Total Salary',max(SALARY)-min(SALARY)  from hr.employees group by JOB_ID;
+
+#24. select JOB_ID from hr.job_history where END_DATE-START_DATE>300 group by JOB_ID having count(*)>=2;
+
+#25. select COUNTRY_ID,count(*) as 'Number of Cities' from hr.location group by COUNTRY_ID;
+
+#26. select MANAGER_ID,count(*) as 'Number of Employees' from hr.employees group by MANAGER_ID having MANAGER_ID>0;
+
+#27. select * from hr.jobs order by JOB_TITLE desc;
+
+#28. select FIRST_NAME,LAST_NAME,HIRE_DATE from hr.employees where JOB_ID in ('SA_MAN','SA_REP');
+
+#29. select DEPARTMENT_ID,avg(SALARY) as 'Average Salary'  from hr.employees  where COMMISSION_PCT is not null group by DEPARTMENT_ID;
+
+#30. select DEPARTMENT_ID from hr.employees group by DEPARTMENT_ID,MANAGER_ID having count(EMPLOYEE_ID)>=4;
+
+#31. select DEPARTMENT_ID from hr.employees where COMMISSION_PCT is not null group by DEPARTMENT_ID having count(*)>10 ;
+
+#32. SELECT employee_id , MAX(end_date) FROM job_history WHERE employee_id IN (SELECT employee_id FROM job_history GROUP BY 1 HAVING COUNT(employee_id) > 1) GROUP BY 1
+
+#33. select * from hr.employees where COMMISSION_PCT is  null and SALARY between 7000 and 12000 and DEPARTMENT_ID=50;
+
+#34. select JOB_ID, avg(SALARY) from hr.employees group by JOB_ID having  avg(SALARY)>8000;
+
+#35. select JOB_TITLE,MAX_SALARY-MIN_SALARY from hr.jobs where MAX_SALARY between 12000 and 18000;
+
+#36. select FIRST_NAME,LAST_NAME from hr.employees where FIRST_NAME like 'D%' or LAST_NAME like'D%';
+
+#37. select * from hr.jobs where MIN_SALARY>9000;
+
+#38. select * from hr.employees where HIRE_DATE>'1987-09-07';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
 
 
 
